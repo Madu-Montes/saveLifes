00cc80dc67a881d2a5f52c9e131ccb18ce1f998c
Madu-Montes <monteseduarda56@gmail.com>
2024-02-15 20:51:27 -0300

    finish

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,33 @@
 document.getElementById('donation-form').addEventListener('submit', function(event) {
-    var name = document.getElementById('name').value;
-    var dob = new Date(document.getElementById('dtn').value);
-    var today = new Date();
-    var age = today.getFullYear() - dob.getFullYear();
-    var cpf = document.getElementById('cpf').value;
-    var weight = document.getElementById('weight').value;
-  
-    // Verifica se a idade é maior que 16 anos
-    if (age < 16) {
-      alert('Você precisa ter mais de 16 anos para doar sangue.');
-      event.preventDefault();
-      return;
-    }
-  
-    // Verifica se o peso é maior que 50 kg
-    if (weight < 50) {
-      alert('Você precisa ter mais de 50 kg para doar sangue.');
-      event.preventDefault();
-      return;
-    }
-  
-    // Se todas as validações passarem, o formulário será enviado normalmente
-  });
-  
+  var cpfInput = document.getElementById('cpf');
+  var cpf = cpfInput.value.replace(/\D/g, ''); // Remove caracteres não numéricos do CPF
+  if (cpf.length !== 11) {
+    alert('CPF inválido! Deve conter 11 dígitos.');
+    event.preventDefault(); 
+    return; 
+  }
+
+  var birthDateInput = document.getElementById('dtn');
+  var birthDate = new Date(birthDateInput.value); //pega a dtn
+  var today = new Date(); //pega a dt atual
+  var age = today.getFullYear() - birthDate.getFullYear(); // o ano atual - e o ano de nanscimento 
+  var monthDiff = today.getMonth() - birthDate.getMonth(); //o mes atual - e o mes de nanscimento 
+  if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+    age--; // verifica se já fez aniversário este ano
+  }
+
+  if (age < 16) {
+    alert('Você precisa ter pelo menos 16 anos para doar sangue.');
+    event.preventDefault(); 
+    return; 
+  }
+
+  var weightInput = document.getElementById('weight');
+  var weight = parseFloat(weightInput.value);
+  if (weight < 50) {
+    alert('Você precisa ter pelo menos 50kg para doar sangue.');
+    event.preventDefault(); 
+    return; 
+  }
+
+});
